aebee9245ad53263ec119d57d3124a93a328355d
Ben <salzig+github@gmail.com>
2012-05-08 15:23:33 +0300

    remove bogus imports

--- src/haw/ai/rn/NonBlockingServer.java
+++ src/haw/ai/rn/NonBlockingServer.java
@@ -2,8 +2,6 @@ package haw.ai.rn;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
